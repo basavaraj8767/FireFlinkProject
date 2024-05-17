28afe57a6847a5787476d950782ed6f71edf7e05
Basavaraj <Basavaraj@TYSS-Basavaraj>
2024-05-17 12:53:48 +0530

    MOdified the GitTest Class

--- Fireflink/src/test/java/ty1/GitTest.java
+++ Fireflink/src/test/java/ty1/GitTest.java
@@ -3,5 +3,8 @@ package ty1;
 public class GitTest {
 	public static void main(String[] args) {
 		//Git Pull Request Test
+		System.out.println("Login to Git");
+		System.out.println("Create new Repo in Git");
+		System.out.println("Verify the Account");
 	}
 }
