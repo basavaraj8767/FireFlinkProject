8f12c63c6c001dbc873a4801a696945c5eba38e2
Basavaraj <Basavaraj@TYSS-Basavaraj>
2024-05-18 14:55:21 +0530

    update

--- Fireflink/src/test/java/ty1/GitTest.java
+++ Fireflink/src/test/java/ty1/GitTest.java
@@ -3,14 +3,14 @@ package ty1;
 public class GitTest {
 	public static void main(String[] args) {
 		// Git Pull Request Test
-
+		
 		String repoName = "TY";
 		System.out.println("Open Browser");
 		System.out.println("Navigate to URL");
 		System.out.println("Login to Git");
 		System.out.println("Create new Repo in Git with name "+repoName);
 		System.out.println("Verify the Account");
-		System.out.println("Logout");
+		System.out.println("Logout from App");
 		System.out.println("Close Browser");
 	}
 }
